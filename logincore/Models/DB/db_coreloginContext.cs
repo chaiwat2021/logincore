f8c07a3f3dc2b6685596a7b1478d4324265fbe44
chaiwat2021 <79843590+chaiwat2021@users.noreply.github.com>
2021-03-16 23:20:38 +0700

    Install Library Packages

---
+++ logincore/Models/DB/db_coreloginContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace logincore.Models.DB
+{
+    public partial class db_coreloginContext : DbContext
+    {
+        public db_coreloginContext()
+        {
+        }
+
+        public db_coreloginContext(DbContextOptions<db_coreloginContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Login> Login { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=serverAddress;Database=dbName;Trusted_Connection=True;user id=dbUsername;password=dbPassword;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Login>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasColumnName("username")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
